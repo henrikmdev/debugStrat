6098be9f8a95096db5154e5ec042c1908f8b6257
Henrik <henrik@henrikmdev.com>
2024-12-17 23:53:33 -0500

    Implement print_array function

--- debugStrat.c
+++ debugStrat.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+void print_array(int arr[], int size) {
+    //printf("The size of the array is %d\n", size);
+    for (int i = 0; i < size; i++) {
+        //printf("numbers[%d] - ", i);
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int main() {
     int numbers[] = {1, 2, 3, 4, 5};
     int size = sizeof(numbers) / sizeof(numbers[0]);
-    for (int i = 0; i < size; i++) {
-        printf("%d ", numbers[i]);
-    }
-    printf("\n");
+    print_array(numbers, size);
     return 0;
 }
